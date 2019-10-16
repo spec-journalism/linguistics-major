3f87838fc16a7d4e984c612690b9ab321288c40b
Jason Kao <jason.kao@columbia.edu>
2019-10-16 13:10:04 -0400

    linguistics 3

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -340,7 +340,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63398" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49676" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -517,3 +517,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","script.js"], "script")
+//# sourceMappingURL=/script.75da7f30.js.map
