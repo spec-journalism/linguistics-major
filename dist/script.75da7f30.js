d5361445c4daa1411c48f0e4cb5db737b327dd21
Jason Kao <jason.kao@columbia.edu>
2019-10-16 10:52:19 -0400

    variables work

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -340,7 +340,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63006" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63398" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -517,4 +517,3 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","script.js"], "script")
-//# sourceMappingURL=/script.75da7f30.js.map
