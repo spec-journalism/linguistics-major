9ad6cd45609d59cd36caee5ad5c0e15b00a9dc7b
Jason Kao <jason.kao@columbia.edu>
2019-10-16 17:16:09 -0400

    linguistics 1

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -340,7 +340,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58806" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60930" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
